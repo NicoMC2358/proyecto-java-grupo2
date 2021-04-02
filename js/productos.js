63c8d626b28838166db0fafd4e12a47aa9247ab6
Rekke-uk <mazcm2013@gmail.com>
2021-04-02 17:31:23 -0300

    funcionalidad del carrito

--- js/productos.js
+++ js/productos.js
@@ -17,14 +17,14 @@ let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 //Variable que suma los precios del carrito
 let sumaCarrito = 0;
 
-// //Contador del badge de carrito
-// let contadorCarrito = document.querySelector("#countCarrito");
+//Contador del badge de carrito
+let contadorCarrito = document.querySelector("#contadorCarrito");
 
-//Contenedor card-deck
+//Contenedor de las Cards de los Productos
 let contenedor = document.querySelector("#cardsContenedor");
 
-// //Body del modal
-// let cuerpoModal = document.querySelector(".modal-body");
+//Body del modal
+let cuerpoModal = document.querySelector(".modal-body");
 
 function agregarProducto() {
     let veces = parseInt(prompt("¿Cuantos productos va a ingresar?"));
@@ -72,6 +72,15 @@ function agregarProducto() {
     cargarCard()
 }
 
+function cantidadCarrito() {
+    let sumaCantidad = 0;
+
+    for (let i = 0; i < carrito.length; i++) {
+        sumaCantidad += carrito[i].cantidad;
+    }
+    contadorCarrito.innerHTML = sumaCantidad;
+}
+
 function cargarCard() {
     contenedor.innerHTML = "";
     for (let i = 0; i < productos.length; i++) {
@@ -93,3 +102,103 @@ function cargarCard() {
         contenedor.appendChild(div);
     }
 }
+
+function agregarCarrito(codigo) {
+    let indexProd = productos.findIndex(function (prod) {
+        return prod.codigo === codigo;
+    });
+
+    if (productos[indexProd].stock >= 1) {
+        productos[indexProd].stock -= 1;
+
+        let indexCar = carrito.findIndex(function (prod) {
+            return prod.id === codigo;
+        });
+
+        if (indexCar >= 0) {
+            carrito[indexCar].cantidad += 1;
+            carrito[indexCar].precio += carrito[indexCar].precio;
+        } else {
+            carrito.push({
+                id: productos[indexProd].codigo,
+                nombre: productos[indexProd].nombre,
+                precio: productos[indexProd].precio,
+                imagen: productos[indexProd].imagen,
+                cantidad: 1,
+            });
+        }
+
+        localStorage.setItem("carrito", JSON.stringify(carrito));
+        localStorage.setItem("productos", JSON.stringify(productos));
+        cantidadCarrito();
+        cargarCard();
+    } else {
+        alert("No hay stock disponible para este producto");
+    }
+}
+
+function cargarModal() {
+    cuerpoModal.innerHTML = "";
+
+    let suma = document.querySelector("#totalCarrito");
+    sumaCarrito = 0;
+
+    if (carrito.length === 0) {
+        cuerpoModal.innerHTML = "<h4>No hay productos en el carrito</h4>";
+        sumaCarrito = 0;
+        suma.innerHTML = `<b>${sumaCarrito}</b>`;
+        return;
+    }
+
+    carrito.forEach(function (prod) {
+        let div = document.createElement("div");
+        div.classList = "card mb-3";
+        let detalle = `
+            <div class="row no-gutters">
+                <div class="col-md-4">
+                    <img class="imagenCarrito" src="${prod.imagen}" alt="${prod.nombre}">
+                </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                <h5 class="card-title">${prod.cantidad} ${prod.nombre}</h5>
+                <p class="card-text">Precio: $${prod.precio}</p>
+                <a href="#" class="btn btn-danger" onclick="eliminarProdCarrito(${prod.id})" >Eliminar</a>
+                </div>
+            </div>
+        </div>
+    `;
+        div.innerHTML = detalle;
+        cuerpoModal.appendChild(div);
+        sumaCarrito += prod.precio;
+    });
+
+    suma.innerHTML = `<b>$${sumaCarrito}</b>`;
+}
+
+function verCarrito() {
+    cargarModal();
+    $("#modalCarrito").modal("show");
+}
+
+function eliminarProdCarrito(id) {
+    let index = carrito.findIndex(function (prod) {
+        return prod.id === id;
+    });
+
+    let cantidad = carrito[index].cantidad;
+    carrito.splice(index, 1);
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+
+    let indexProd = productos.findIndex(function (prod) {
+        return prod.codigo === id;
+    });
+    productos[indexProd].stock += cantidad;
+    localStorage.setItem("productos", JSON.stringify(productos));
+
+    cargarCard();
+    cargarModal();
+    cantidadCarrito();
+}
+
+cargarCard();
+cantidadCarrito();
